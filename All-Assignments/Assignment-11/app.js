0c71f3951ebac62ffe7b76d0de9884236fe93487
Abdullah Bin Abdul Qadeer <abdullahaqr@users.noreply.github.com>
2020-08-12 19:05:46 +0500

    update countdown error

--- All-Assignments/Assignment-11/app.js
+++ All-Assignments/Assignment-11/app.js
@@ -1,60 +1,77 @@
 var questionsArray = [
     {
         question: "Full Form of RAM is ?",
-        answer: "random access memory",
+        answer: "Random Access Memory",
         options: [
-            "random access memory",
-            "random actual memory",
-            "right access memory",
-            "none of the above",
-        ]
-    },
-    {
-        question: "Full Form of CPU is ?",
-        answer: "central processing unit",
-        options: [
-            "central program unit",
-            "central processing unit",
-            "control panel unit",
-            "none of the above",
-        ]
-    },
-    {
-        question: "Full Form of E-MAIL is ?",
-        answer: "electronic mail",
-        options: [
-            "electric mail",
-            "easy mail",
-            "electronic mail",
-            "none of the above",
+            "Random Access Memory",
+            "Random Actual Memory",
+            "Right Access Memory",
+            "None of the above",
         ]
     },
     {
         question: "Full Form of LCD is ?",
-        answer: "liquid crystal display",
+        answer: "Liquid Crystal Display",
         options: [
-            "liquid crystal display",
-            "light crystal display",
-            "liquid cold display",
-            "none of the above",
+            "Liquid Crystal Display",
+            "Light Crystal Display",
+            "Liquid Cold Display",
+            "None of the above",
+        ]
+    },
+    {
+        question: "Full Form of E-MAIL is ?",
+        answer: "Electronic Mail",
+        options: [
+            "Electric Mail",
+            "Easy Mail",
+            "Electronic Mail",
+            "None of the above",
+        ]
+    },
+    {
+        question: "Full Form of ATM is ?",
+        answer: "Automated Teller Machine",
+        options: [
+            "Auto Transmission Method",
+            "Automatic Transition Money",
+            "Automated Teller Machine",
+            "None of the above",
         ]
     },
     {
         question: "Full Form of SEO is ?",
-        answer: "none of the above",
+        answer: "None of the above",
         options: [
-            "safe engine optimization",
-            "secure engine optimization",
-            "secret engine optimization",
-            "none of the above",
+            "Safe Engine Optimization",
+            "Secure Engine Optimization",
+            "Secret Engine Optimization",
+            "None of the above",
         ]
     },
 ];
 
+var questionCount = 0;
+var score = 0;
+var attemptAnswer = 0;
+var correctAnswer = 0;
+var wrongAnswer = 0;
+var percentageAnswer = 0;
+var nameTry = "";
+var nameCheck = "";
+
+var minute = 2;
+var sec = 00;
+var interval;
+var minSpan = document.getElementById("minSpan")
+var secSpan = document.getElementById("secSpan")
+var timer = document.getElementById("timer")
+
 
 function startQuiz() {
     var name = document.getElementById("name")
-    if (name.value == "") {
+    nameCheck = name.value
+    if (nameCheck == "") {
         alert("Please Enter Your Name !")
     }
     else {
@@ -64,40 +81,60 @@ function startQuiz() {
         var quiz = document.getElementById("quiz")
         quiz.classList.remove("d-none")
         showQuestion(0)
-
+        interval = setInterval(timerFunc, 1000)
     }
 
+}
+function tryAgain() {
+    var result = document.getElementById("result")
+    result.classList.add("d-none")
+
+    var quiz = document.getElementById("quiz")
+    quiz.classList.remove("d-none")
+
+    removeActiveClass()
+    showQuestion(0)
+
+    var name = document.getElementById("name")
+    name.value = nameTry
+
+    interval = setInterval(timerFunc, 1000)
 }
 
 function showQuestion(e) {
     questionNumber(questionCount)
-    countDownTimer()
 
-    // show q
+    // To show question
     var questionElement = document.getElementById("questionElement")
     questionElement.innerHTML = "Q." + (questionCount + 1) + " : " + questionsArray[e].question
 
-    // show option
+    // To show option
     var optionElement = document.getElementsByClassName("optionElement")
     for (var i = 0; i < optionElement.length; i++) {
         optionElement[i].innerHTML = questionsArray[e].options[i]
     }
 }
 
-var questionCount = 0;
-var score = 0;
-
 function nextQuestion() {
-    validate(questionCount)
-    showQuestion(questionCount)
-    removeActiveClass()
+    if (questionCount <= 4) {
+        validate(questionCount)
 
+        if (questionCount < 5) {
+            showQuestion(questionCount)
+            removeActiveClass()
+        }
+        if (questionCount == 5) {
+            showResult()
+        }
+
+    }
 }
 
 function putActiveClass(e) {
     removeActiveClass()
     e.classList.add("active")
 }
+
 function removeActiveClass() {
     var active = document.getElementsByClassName("active")
     for (var i = 0; i < active.length; i++) {
@@ -106,7 +143,6 @@ function removeActiveClass() {
 }
 
 function validate(e) {
-
     if (document.querySelectorAll("p.active").length == 0) {
         alert("Please select one option !")
     }
@@ -114,8 +150,13 @@ function validate(e) {
         var active = document.querySelector("p.active")
         if (active.innerHTML == questionsArray[e].answer) {
             score += 10
+            attemptAnswer += 1
+            correctAnswer += 1
             questionCount++;
-            console.log(score)
+        } else {
+            attemptAnswer += 1
+            wrongAnswer += 1
+            questionCount++;
         }
     }
 }
@@ -123,45 +164,97 @@ function validate(e) {
 function questionNumber(e) {
     var header = document.getElementById("header")
     header.innerHTML = "Question # " + (e + 1) + " / 5"
-
-    console.log(header.innerHTML)
 }
 
+function timerFunc() {
+    sec = (Math.abs(sec))
+    minute = (Math.abs(minute))
 
-function countDownTimer() {
-    var minute = 2;
-    var sec = 00;
-    var minSpan = document.getElementById("minSpan")
-    var secSpan = document.getElementById("secSpan")
-    var timer = document.getElementById("timer")
-    function timerFunc() {
-
-        
-        if (sec == 00) {
-            minute--;
-            sec = 60;
-        }
-        sec--;
-
-        if (sec < 10) {
-            secSpan.innerHTML = "0" + sec.toString();
-        }else{
-            secSpan.innerHTML =  sec;
-        }
-        if (minute < 10) {
-            minSpan.innerHTML = "0" + minute.toString();
-        }else{
-            minSpan.innerHTML =  minute;
-        }
-
-        if(minute == 00 && sec == 20){
-            timer.classList.add("timerRed")
-        }
-
-        if(minute == 00 && sec == 00){
-            timer.classList.remove("timerRed")
-            alert("Oopps!! Time's Up !!!");
-        }
+    if (sec == 00) {
+        minute--;
+        sec = 60;
     }
-    setInterval(timerFunc, 1000);
+    sec--;
+
+    if (sec < 10) {
+        secSpan.innerHTML = "0" + sec.toString();
+    } else {
+        secSpan.innerHTML = sec;
+    }
+    if (minute < 10) {
+        minSpan.innerHTML = "0" + minute.toString();
+    } else {
+        minSpan.innerHTML = minute;
+    }
+
+    if (minute == 00 && sec == 20) {
+        timer.classList.add("timerRed")
+    }
+
+    if (minute == 00 && sec == 00) {
+        timer.classList.remove("timerRed")
+        alert("Oopps!! Time's Up !!!");
+        showResult()
+    }
+}
+
+function showResult() {
+    var quiz = document.getElementById("quiz")
+    quiz.classList.add("d-none")
+
+    var result = document.getElementById("result")
+    result.classList.remove("d-none")
+
+    percentageAnswer = ((score / 50) * 100)
+
+    var studentName = document.getElementById("studentName")
+    var name = document.getElementById("name")
+    var attempt = document.getElementById("attempt")
+    var correct = document.getElementById("correct")
+    var wrong = document.getElementById("wrong")
+    var percentage = document.getElementById("percentage")
+    var totalScore = document.getElementById("totalScore")
+
+    studentName.innerHTML = name.value
+    attempt.innerHTML = attemptAnswer
+    correct.innerHTML = correctAnswer
+    wrong.innerHTML = wrongAnswer
+    percentage.innerHTML = percentageAnswer + " %"
+    totalScore.innerHTML = score + " / 50"
+
+    questionCount = 0;
+    score = 0;
+    correctAnswer = 0;
+    wrongAnswer = 0;
+    percentageAnswer = 0;
+
+    nameTry = name.value
+
+    name.value = ""
+
+    resetTimer()
+}
+
+function goToHome() {
+    var result = document.getElementById("result")
+    result.classList.add("d-none")
+
+    var welcome = document.getElementById("welcome")
+    welcome.classList.remove("d-none")
+
+    questionCount = 0;
+    score = 0;
+    correctAnswer = 0;
+    wrongAnswer = 0;
+    percentageAnswer = 0;
+
+}
+
+function resetTimer(){
+
+    clearInterval(interval);
+    minute = 0;
+    sec = 00;
+    secSpan.innerHTML = 00;
+    minSpan.innerHTML = 02;
 }
